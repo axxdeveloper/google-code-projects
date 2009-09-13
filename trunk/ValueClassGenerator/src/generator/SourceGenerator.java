02d0372fecce0c179180701bca72b2b8df83de84
shooeugenesea.tw <shooeugenesea.tw@933a0ec0-a08d-11de-98a9-4d1d5fda2735>
2009-09-13 18:14:36 +0000

    add license words

--- trunk/ValueClassGenerator/src/generator/SourceGenerator.java
+++ trunk/ValueClassGenerator/src/generator/SourceGenerator.java
@@ -1,4 +1,20 @@
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package generator;
 
 public class SourceGenerator {
@@ -59,6 +75,7 @@ public class SourceGenerator {
         src.append(indentation(2) + "public " + cls.getName() + " build() {" + LINE_SEP);
         src.append(indentation(3) + "return new " + cls.getName() + "(this);" + LINE_SEP);
         src.append(indentation(2) + "}" + LINE_SEP);
+        src.append(LINE_SEP);
         src.append(indentation(1) + "}" + LINE_SEP);
         src.append(LINE_SEP);
         src.append(indentation(0) + "}" + LINE_SEP);
